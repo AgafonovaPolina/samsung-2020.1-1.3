f481851b8d7463a18c60e244229e8ab7d728e0ef
AgafonovaPolina <74154230+AgafonovaPolina@users.noreply.github.com>
2021-03-18 16:55:36 +0400

    Update MyProgram.java

--- MyProgram.java
+++ MyProgram.java
@@ -1,3 +1,4 @@
+<?xml version="1.0" encoding="utf-8"?>
 <androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:app="http://schemas.android.com/apk/res-auto"
     xmlns:tools="http://schemas.android.com/tools"
@@ -6,26 +7,26 @@
     tools:context=".MainActivity">
 
     <TextView
-        android:id="@+id/textView4"
-        android:layout_width="366dp"
-        android:layout_height="89dp"
-        android:background="#3FFA47"
-        android:text="Добавте комментарий"
-        app:autoSizeTextType="uniform"
-        tools:layout_editor_absoluteX="22dp"
-        tools:layout_editor_absoluteY="244dp"
-        tools:ignore="MissingConstraints" />
-
-    <TextView
         android:id="@+id/textView5"
         android:layout_width="366dp"
         android:layout_height="89dp"
         android:background="#59F660"
         android:text="Установите время"
         app:autoSizeTextType="uniform"
+        tools:ignore="MissingConstraints"
+        tools:layout_editor_absoluteX="21dp"
+        tools:layout_editor_absoluteY="211dp" />
+
+    <TextView
+        android:id="@+id/textView7"
+        android:layout_width="366dp"
+        android:layout_height="89dp"
+        android:background="#59F660"
+        android:text="Введите название препарата"
+        app:autoSizeTextType="uniform"
+        tools:ignore="MissingConstraints"
         tools:layout_editor_absoluteX="22dp"
-        tools:layout_editor_absoluteY="51dp"
-        tools:ignore="MissingConstraints" />
+        tools:layout_editor_absoluteY="2dp" />
 
     <EditText
         android:id="@+id/editTextTime"
@@ -33,9 +34,30 @@
         android:layout_height="69dp"
         android:ems="10"
         android:inputType="time"
+        tools:ignore="MissingConstraints"
         tools:layout_editor_absoluteX="22dp"
-        tools:layout_editor_absoluteY="140dp"
-        tools:ignore="MissingConstraints" />
+        tools:layout_editor_absoluteY="300dp" />
+
+    <EditText
+        android:id="@+id/editTextTime3"
+        android:layout_width="367dp"
+        android:layout_height="69dp"
+        android:ems="10"
+        android:inputType="time"
+        tools:ignore="MissingConstraints"
+        tools:layout_editor_absoluteX="22dp"
+        tools:layout_editor_absoluteY="91dp" />
+
+    <TextView
+        android:id="@+id/textView4"
+        android:layout_width="366dp"
+        android:layout_height="89dp"
+        android:background="#3FFA47"
+        android:text="Добавте комментарий"
+        app:autoSizeTextType="uniform"
+        tools:ignore="MissingConstraints"
+        tools:layout_editor_absoluteX="22dp"
+        tools:layout_editor_absoluteY="416dp" />
 
     <EditText
         android:id="@+id/editTextTime2"
@@ -43,19 +65,21 @@
         android:layout_height="69dp"
         android:ems="10"
         android:inputType="time"
-        tools:layout_editor_absoluteX="22dp"
-        tools:layout_editor_absoluteY="335dp"
-        tools:ignore="MissingConstraints" />
+        tools:ignore="MissingConstraints"
+        tools:layout_editor_absoluteX="21dp"
+        tools:layout_editor_absoluteY="505dp" />
 
     <Button
         android:id="@+id/button5"
         android:layout_width="410dp"
         android:layout_height="119dp"
         android:text="Закончить"
+        android:textAppearance="@style/TextAppearance.AppCompat.Display3"
+        android:visibility="visible"
         app:backgroundTint="#0FE418"
+        tools:ignore="MissingConstraints"
         tools:layout_editor_absoluteX="1dp"
-        tools:layout_editor_absoluteY="612dp"
-        tools:ignore="MissingConstraints" />
+        tools:layout_editor_absoluteY="612dp" />
 
 </androidx.constraintlayout.widget.ConstraintLayout>
  
